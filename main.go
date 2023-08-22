810c6251b0be9509aada9ccdc69d086b5656939d
Christopher Dykes <chris.t.c.dykes@gmail.com>
2023-08-22 13:10:36 -0400

    gofmt

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/gocolly/colly"
 	"math/rand"
 	"time"
-	"github.com/gocolly/colly"
 )
 
 func main() {
@@ -15,41 +15,41 @@ func main() {
 		r.Headers.Set("User-Agent", userAgent)
 	})
 
-    years := visitFirstPage(c) // Get years and scrape the data
-    for _, year := range years {
-        fmt.Println("Searching from year: ", year)
-        visitPageByYear(year, c)
-        sleepLong()
-    }
+	years := visitFirstPage(c) // Get years and scrape the data
+	for _, year := range years {
+		fmt.Println("Searching from year: ", year)
+		visitPageByYear(year, c)
+		sleepLong()
+	}
 }
 
 func visitFirstPage(c *colly.Collector) []string {
-    var years []string
-    c.OnHTML("#changeY", func (e *colly.HTMLElement) {
-        years = e.ChildAttrs("option", "value")
-    })
+	var years []string
+	c.OnHTML("#changeY", func(e *colly.HTMLElement) {
+		years = e.ChildAttrs("option", "value")
+	})
 
-    c.OnHTML(".type-a", getFigureLinks)
-    
-    sleepShort()
+	c.OnHTML(".type-a", getFigureLinks)
+
+	sleepShort()
 	url := fmt.Sprintf("https://alter-web.jp/figure")
 	c.Visit(url)
 
-    return years
+	return years
 }
 
 func visitPageByYear(year string, c *colly.Collector) {
 	c.OnHTML(".type-a", getFigureLinks)
-    sleepShort()
+	sleepShort()
 	url := fmt.Sprintf("https://alter-web.jp/figure/?yy=%s&mm=", year)
 	c.Visit(url)
 }
 
 func getFigureLinks(e *colly.HTMLElement) {
 	links := e.ChildAttrs("a", "href")
-    for _, link := range links {
-        fmt.Println("Found figure: ", link)
-    }
+	for _, link := range links {
+		fmt.Println("Found figure: ", link)
+	}
 }
 
 func sleepShort() {
@@ -58,7 +58,6 @@ func sleepShort() {
 }
 
 func sleepLong() {
-	randomNumber := rand.Float64()*(10-5) + 5 
+	randomNumber := rand.Float64()*(10-5) + 5
 	time.Sleep(time.Duration(randomNumber) * time.Second)
 }
-
